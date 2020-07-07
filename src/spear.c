296308e7ea9d0d288edb4087768ce93801b266ac
Kodai Yokoyama <aaaaa333445@gmail.com>
2020-07-07 11:19:03 +0900

    revised

--- src/spear.c
+++ src/spear.c
@@ -4,6 +4,15 @@ int n;
 int k;
 int A[100000];
 
+int p(unsigned int x)
+{
+  int sum;
+  sum = 0;
+  for (int i = 0; i < n; i++)
+    sum = sum + A[i] / x;
+  return sum >= k;
+}
+
 
 int main(){
   int i, lb, ub;
@@ -11,7 +20,22 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
+  lb = 0;
+  ub = 1000000000;
 
+  while (ub - lb > 1)
+  {
+    int mid = (lb + ub) / 2;
+    if (p(mid))
+    {
+      lb = mid;
+    }
+    else
+    {
+      ub = mid;
+    }
+  }
+  printf("%d\n", lb);
 
   return 0;
 }
